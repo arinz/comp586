738405b619c427554b4a40cad9bdb51b78429ee0
arinz <arin.zargarian.373@my.csun.edu>
2019-12-16 08:59:35 -0800

    Add files via upload

---
+++ backend/backend/Models/Quiz.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models
+{
+    public class Quiz
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string OwnerId { get; set; }
+    }
+}
