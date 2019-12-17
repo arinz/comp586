a90b71cea18ad55da93cec3b2bc71ba98ec44820
arinz <arin.zargarian.373@my.csun.edu>
2019-12-17 11:28:30 -0800

    Add files via upload

---
+++ backend/backend/Models/Owner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Owner
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
